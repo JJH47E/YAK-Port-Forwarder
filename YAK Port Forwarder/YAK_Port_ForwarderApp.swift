7d4f2142e9ad29decc46db597dd5eb6742fe59e2
JJH <62762383+JJH47E@users.noreply.github.com>
2025-06-29 12:56:20 +0100

    Initial Commit

---
+++ YAK Port Forwarder/YAK_Port_ForwarderApp.swift
@@ -0,0 +1,17 @@
+//
+//  YAK_Port_ForwarderApp.swift
+//  YAK Port Forwarder
+//
+//  Created by JJ Hayter on 29/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct YAK_Port_ForwarderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
